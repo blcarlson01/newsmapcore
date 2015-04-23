0eab4f0a4f69442b13470dc38dc58f4730ea8b20
blcarlson <blcarlson@192.168.10.4>
2015-04-22 20:01:36 -0400

    working for single file outputing to sysout in json format

--- NewsMapCore/src/sentiment/SentimentAnalyzer.java
+++ NewsMapCore/src/sentiment/SentimentAnalyzer.java
@@ -1,10 +1,22 @@
 package sentiment;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.lang3.time.StopWatch;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import news.Article;
 import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.neural.rnn.RNNCoreAnnotations;
 import edu.stanford.nlp.pipeline.Annotation;
@@ -13,124 +25,154 @@ import edu.stanford.nlp.sentiment.SentimentCoreAnnotations;
 import edu.stanford.nlp.trees.Tree;
 import edu.stanford.nlp.util.CoreMap;
 
-
-
 /**
  * @author blcarlson
  *
  */
 public class SentimentAnalyzer {
+
+	public static List<Sentiment> findSentiment(String line) {
+
+		Properties props = new Properties();
+		props.setProperty("annotators", "tokenize, ssplit, parse, sentiment");
+		StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+		List<Sentiment> sentimentList = new ArrayList<Sentiment>();
+
+		if (line != null && line.length() > 0) {
+			Annotation annotation = pipeline.process(line);
+			for (CoreMap sentence : annotation
+					.get(CoreAnnotations.SentencesAnnotation.class)) {
+				Tree tree = sentence
+						.get(SentimentCoreAnnotations.AnnotatedTree.class);
+				int sentiment = RNNCoreAnnotations.getPredictedClass(tree);
+				String partText = sentence.toString();
+
+				// String[] sentimentText = { "Very Negative","Negative",
+				// "Neutral", "Positive", "Very Positive"};
+				// System.out.println(partText + " = "+ sentimentText[sentiment]
+				// + " length: "+ partText.length());
+
+				sentimentList.add(new Sentiment(sentiment, partText.length(),
+						sentence.toString()));
+			}
+		}
+
+		return sentimentList;
+
+	}
 	
-	 public static List<Sentiment> findSentiment(String line) {
-		 
-	        Properties props = new Properties();
-	        props.setProperty("annotators", "tokenize, ssplit, parse, sentiment");
-	        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
-	        List<Sentiment> sentimentList = new  ArrayList<Sentiment>();	        
-	        
-	        if (line != null && line.length() > 0) {
-	        	//int longest = 0;
-	            Annotation annotation = pipeline.process(line);
-	            for (CoreMap sentence : annotation
-	                    .get(CoreAnnotations.SentencesAnnotation.class)) {
-	                Tree tree = sentence
-	                        .get(SentimentCoreAnnotations.AnnotatedTree.class);
-	                int sentiment = RNNCoreAnnotations.getPredictedClass(tree);
-	                String partText = sentence.toString();
-	                
-	               // String[] sentimentText = { "Very Negative","Negative", "Neutral", "Positive", "Very Positive"};
-	               // System.out.println(partText + " = "+ sentimentText[sentiment] + " length: "+ partText.length());
-	                
-	                sentimentList.add(new Sentiment(sentiment, partText.length(), sentence.toString()));
-	             /* 
-	              * Keep for now, but should be removed b/c it give too much weight to the longest sentence  
-	              * if (partText.length() > longest) {
-	                    mainSentiment = sentiment;
-	                    longest = partText.length();
-	                }*/
+	@SuppressWarnings("resource")
+	private static List<Article> loadTextFiles(String path)
+	{
+		int xx = 0;
+		final File folder = new File(path);
+		List<Article> articles = new ArrayList<Article>();		
+		try {
+			List<List<String>> files = new ArrayList<List<String>>();
+			List<String> fileContents = new ArrayList<String>();
+			for (final File fileEntry : folder.listFiles()) {
+				if (fileEntry.getName().endsWith("txt")) {
+					File textFile = new File(path + fileEntry.getName());
+					FileReader fr = new FileReader(textFile);
+					BufferedReader br = new BufferedReader(fr);
+					String line;
+					fileContents.add(textFile.getName());
+					while ((line = br.readLine()) != null) {
+						fileContents.add(line);
+					}
 
-	            }
-	        }
-	 	        
-	        return sentimentList;
-	 
-	    }
-	 
-	public static void main(String[] args) throws IOException {
-		
-		String line = "MOSCOW (Reuters) - Russian authorities said on Sunday they were holding five men over the killing of Kremlin critic Boris Nemtsov, one of whom served in a police unit in the Russian region of Chechnya, according to a law enforcement official.";
-		
-		String line2 = "I love cats. I love every kind of cat. I just want to hug all of them. But I can't. Can't hug every cat. Can't hug every cat. So anyway. I am a cat lover and I love to run";
-		String line3 = "Demanding a summit on the issue, Mr Renzi said trafficking was \"a plague in our continent\" and bemoaned the lack of European solidarity.  The 20m (70ft) long boat was believed to be carrying up to 700 migrants, and only 28 survivors have been rescued.  Up to 1,500 migrants are now feared to have drowned this year alone.  Human smugglers are taking advantage of the political crisis in Libya to use it as a launching point for boats carrying migrants who are fleeing violence or economic hardship in Africa and the Middle East.";
-		
-		String[] sentimentText = { "Very Negative","Negative", "Neutral", "Positive", "Very Positive"};
-		
-		//System.out.println("Moscow SENT: " + sentimentText[findSentiment(line)]);
-		//System.out.println("Cats SENT: " + sentimentText[findSentiment(line2)]);
-		//System.out.println("Death SENT: " + sentimentText[findSentiment(line3)]);
+					files.add(fileContents);
+					fileContents = new ArrayList<String>();
+					if (xx == 0) {
+						break;
+					}
+					xx++;
+				}
+			}
+						
+			for (int x = 0; x < files.size(); x++) {
+				List<String> contents = files.get(x);
+				Article article = new Article();
+				article.setArticleId(contents.get(0));
+				article.setLink(contents.get(1));
+				article.setTitle(contents.get(2));
+				article.setPubdate(contents.get(3));
+				article.setCategory(contents.get(4));
+				article.setDescription(contents.get(5));
+				article.setAuthor(contents.get(6));
+				article.setKeywords(contents.get(7));
+				article.setImagePath(contents.get(8));
+				article.setLocation(contents.get(9));
+				article.setBody(contents.get(10));
+				articles.add(article);
+			}
 
-		//TODO: average score sum of scores / # of sentences
-		
-		//TODO: smoothing of the scores based on sentence length
-		
-		List<Sentiment> sentiments = findSentiment(line3);
-		
-		int totalScore = 0;
-		int longestScore =0 ;
-		int longest = 0;	
-		
-		int totaldoclen = 0;
-		for (Sentiment sentiment : sentiments) {
-			totaldoclen += sentiment.getSentenceLength();			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		float avgdoclen = totaldoclen/sentiments.size();
-		System.out.println("avg lenth: "+avgdoclen);
 		
+		return articles;
+	}
+
+	public static void main(String[] args) throws IOException {	
+		StopWatch stopWatch = new StopWatch();
+		stopWatch.start();		
 		
-		List<Float> weights = new ArrayList<Float>();
-		for (Sentiment sentiment : sentiments) {
-			
-			totalScore += sentiment.getScore();			
-			int doclen = sentiment.getSentenceLength();
-			int score = sentiment.getScore();
-			
-			
-			float b = 0.5f; 
-			float pn = 1-b+b*doclen/avgdoclen;
-			int k = 1;
-			float weight = 0f;
-			if(score != 0)
-			{
-			 weight = (k+1) * score / score + k * pn;
+		Gson gson = new Gson();
+		String path = "/Users/Shared/CrawlerData/text/";				
+		List<Article> articles = loadTextFiles(path);
+		JsonArray articleInfoList = new JsonArray();	
+		for (int i = 0; i < articles.size(); i++) {
+			Article article = articles.get(i);
+			JsonObject articleInfo = new JsonObject();
+			List<Sentiment> sentiments = findSentiment(articles.get(i).getBody());
+			articleInfo.addProperty("articleId", article.getArticleId());					
+			int totalScore = 0;
+			int longestScore = 0;
+			int longest = 0;
+			JsonArray sentences = new JsonArray();
+			for (Sentiment sentiment : sentiments) 
+			{				
+				JsonObject sentence = new JsonObject();
+				totalScore += sentiment.getScore();						
+				
+				sentence.addProperty("sentence", sentiment.getSentence());
+				sentence.addProperty("sentenceLength", sentiment.getSentenceLength());
+				sentence.addProperty("score", sentiment.getScore());		
+								
+				if (sentiment.getSentenceLength() > longest) {
+					longestScore = sentiment.getScore();
+					longest = sentiment.getSentenceLength();
+				}
+				
+				sentences.add(sentence);
 			}
-		
-			System.out.println(sentiment.getSentence() + " : " + sentimentText[score] +" : "+score + " : modified weight: "+ weight+ " : " + doclen);
-			weights.add(weight);
-			
-			if (sentiment.getSentenceLength() > longest) {
-				longestScore = sentiment.getScore();
-                longest =sentiment.getSentenceLength();
-            }
+			/*String[] sentimentText = { "Very Negative", "Negative", "Neutral",
+					"Positive", "Very Positive" };
+			System.out.println("Total Score: " + totalScore
+					+ " | numOfSentences: " + sentiments.size());
+			System.out.println("Longest Sentence Score: " + longestScore
+					+ " => " + sentimentText[longestScore]);
+			System.out.println("Average Score: "
+					+ (float) totalScore / sentiments.size()
+					+ " => "
+					+ sentimentText[Math.round((float) totalScore / sentiments.size())]); */
+			articleInfo.addProperty("averageScore", (float) totalScore / sentiments.size());
+			articleInfo.addProperty("longestSentenceScore", longestScore);
+			articleInfo.addProperty("descriptionScore", 0);
+			articleInfo.add("sentences", sentences);			
+			articleInfoList.add(articleInfo);
 		}
+					
+		System.out.println(gson.toJson(articleInfoList));
 		
-		System.out.println("Total Score: " + totalScore);
-		System.out.println("Longest Sentence Score: "+ longestScore + " => "+sentimentText[longestScore]);
-		System.out.println("Average Score: " + totalScore/sentiments.size() + " => "+sentimentText[totalScore/sentiments.size()]);
-		
-		float totalWeight = 0f;
-		float highestWeight = 0f;
-		for (Float weight : weights) {
-			totalWeight += weight;
-			if(weight > highestWeight)
-			{
-				highestWeight = weight;	
-			}
-		}
-		
-		System.out.println("Average Score from Modified Weights: " + totalWeight/sentiments.size() + " : floor = "
-				+ sentimentText[(int) Math.floor(totalWeight/sentiments.size())] + " : ceiling = " + sentimentText[(int) Math.ceil(totalWeight/sentiments.size())]);
-		System.out.println("Highest Weight: " + highestWeight);
-		
+		stopWatch.stop();
+		System.out.println("Total Files: "+ articles.size());
+	    System.out.println("Time to Complete (Minutes): "+TimeUnit.MILLISECONDS.toSeconds(stopWatch.getTime()));
 	}
 
 }
